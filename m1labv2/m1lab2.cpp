4b6883db4a2cf2d8222e781d7ddaaa582fedbc27
Noel Latsha <noel.latsa.jr@gmail.com>
2025-01-28 22:48:08 -0600

    made module 1 lab v2

---
+++ m1labv2/m1lab2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "***************************" << endl;
+    cout << "*    Lab 1 - COSC-1336    *" << endl;
+    cout << "***************************" << endl;
+    return 0;
+}
