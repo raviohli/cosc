ac5721d81c74cd22b5a03714497770f2e42d8a13
Noel Latsha <noel.latsa.jr@gmail.com>
2025-01-29 09:49:15 -0600

    Data types prog

---
+++ datatypesclass/datatypes.cpp
@@ -0,0 +1,28 @@
+//************************************************************************
+// Author: Noel Latsha
+// Date: 29 Jan 2025
+// Language: C++
+// Assignment: Data types
+// Description: Output various data types
+//************************************************************************
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // declare local variables
+    string myName = "Dr. Kevin Roark";
+    int myAge = 39;
+    float myGPA = 3.95;
+    char myGrade = 'A';
+
+    // Output some variables
+    cout << "Name: " << myName << endl;
+    cout << "Age: " << myAge << "\n";
+    cout << "GPA: " << myGPA << " Grade: " << myGrade << endl;
+    cout << "End of Program!\n";
+
+    return 0;
+
+}
