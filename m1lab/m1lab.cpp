1b3b97b21ccd7b5b0066817a15c6ddd423599e9c
Noel Latsha <noel.latsa.jr@gmail.com>
2025-01-28 22:34:40 -0600

    build file for m1lab added

--- m1lab/m1lab.cpp
+++ m1lab/m1lab.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello, World!\n\n"; // I wasn't 100% sure whether or not it is a double or single space in the quote, so I went for double.
+    cout << "Hello, World!\n" << endl; // I wasn't 100% sure whether or not it is a double or single space in the quote, so I went for double.
     cout << "Quality is never an accident; it is always the result of high intention, sincere effort, intelligent\n";
     cout << "direction and skillful execution; it represents the wise choice of many alternatives.\n\n";
     cout << "-- William A. Foster" << endl;
