d125ec938559555a0865f41c526a3a14ded46d27
Noel <noel.latsha.jr@gmail.com>
2025-01-29 19:06:35 -0600

    finishing the project

--- m1lab/m1lab.cpp
+++ m1lab/m1lab.cpp
@@ -3,7 +3,7 @@
 // Date: 29 Jan 2025
 // Language: C++
 // Assignment: Module 1 quote lab
-// Description:
+// Description: Output a quote to the terminal
 //************************************************************************
 
 
@@ -13,9 +13,11 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello, World!\n" << endl; // I wasn't 100% sure whether or not it is a double or single space in the quote, so I went for double.
-    cout << "Quality is never an accident; it is always the result of high intention, sincere effort, intelligent\n";
-    cout << "direction and skillful execution; it represents the wise choice of many alternatives.\n\n";
+    cout << "Hello, World!" << endl << endl;
+    cout << "Quality is never an accident; it is always the result of "
+         << "high intention, sincere effort, intelligent direction, "
+         << "and skillful execution; it represents the wise choice "
+         << "of many alternatives" << endl << endl;
     cout << "-- William A. Foster" << endl;
     return 0;
 }
