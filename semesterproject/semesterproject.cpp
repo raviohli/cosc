961d9ed8d8e97c8abd530af5d952c57df4708e6e
Noel <noel.latsha.jr@gmail.com>
2025-02-05 23:17:44 -0600

    changed the project

---
+++ semesterproject/semesterproject.cpp
@@ -0,0 +1,40 @@
+//************************************************************************
+// Author: Noel Latsha
+// Date: 30 Jan 2025
+// Language: C++
+// Assignment: Semester Project A - Section 1
+// Description:
+//************************************************************************
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // ****** Variable Section *********
+    string firstName;
+    string lastName;
+
+    // ****** Welcome Section **********
+    // Output welcome header
+    cout << "******************************************" << endl
+         << "*      Programming 1 Course Project      *" << endl
+         << "******************************************" << endl;
+
+    // Prompt user for first name and store
+    cout << "Please enter your first name: "
+    cin >> firstName;
+
+    // Inject a newline for clean output
+    cout << "\n"
+    // Prompt user for last name and store
+    cout << "Please enter your last name: "
+    cin >> lastName;
+
+    // Output a welcome message to the user
+    cout << "Welcome to my course project " << firstName << " \n"
+
+    return 0;
+}
