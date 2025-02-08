b3e78cfab0343bcb4f907eae15d97900be4771d8
Noel Latsha <noel.latsa.jr@gmail.com>
2025-02-07 20:09:12 -0600

    finished week 2 lab

--- assignments/week2lab/week2lab.cpp
+++ assignments/week2lab/week2lab.cpp
@@ -12,18 +12,14 @@ using namespace std;
 int main()
 {
     // Declare local variables
-    float tempF. tempC;
+    float tempF, tempC;
 
     // Prompt the user for temp in F and store
     cout << "Enter the temperature in Fahrenheit: ";
     cin >> tempF;
 
-    // Prompt the user for the temp in C and store
-    cout << "Enter the temperature in Celsius: ";
-    cin >> tempC;
-
     // Perform the math using the conversion formula
-    tempC = (f * 32) * (5/9);
+    tempC = (tempF - 32) * 5.0 / 9.0;
     cout << "The temperature in celsius is: " << tempC;
     return 0;
 }
