7aeccbe63eeec024aa79c4c3698f9b940d563287
Noel Latsha <noel.latsa.jr@gmail.com>
2025-03-28 15:33:10 -0500

    mod 4 lab week 2 p1

---
+++ assignments/module4week2/module4week2.cpp
@@ -0,0 +1,30 @@
+//************************************************************************
+// Author: Noel Latsha
+// Date: 28 Mar 2025
+// Language: C++
+// Assignment: Module 4 - Week 2
+// Description: Create a program to calculate BMI. Write three functions,
+// one to prompt for user weight and height, one to calculate BMI,
+// and one to display the BMI and bmi category to the user.
+//************************************************************************
+#include <iostream>
+using namespace std;
+
+
+// Function prototypes
+double getWeightHeight(double, double);
+double calcBMI(double, double);
+void classifyBMI(double);
+
+int main()
+{
+    // Declare local variables
+    double weight, height, bmi;
+    string category;
+
+    //
+}
+
+/* Function definitions */
+
+// Function to prompt user for weight and height.
